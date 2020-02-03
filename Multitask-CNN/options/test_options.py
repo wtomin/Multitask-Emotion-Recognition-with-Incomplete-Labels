a4d2e884b2cb03634cc9a1bde2c419ecc6880b2f
Didan Deng <33117903+wtomin@users.noreply.github.com>
2020-02-03 21:42:05 +0800

    Add files via upload

---
+++ Multitask-CNN/options/test_options.py
@@ -0,0 +1,15 @@
+from .base_options import BaseOptions
+
+
+class TestOptions(BaseOptions):
+    def initialize(self):
+        BaseOptions.initialize(self)
+        self.is_train = False
+        self._parser.add_argument('--teacher_model_path',default = '', type=str,  help='the model to be evaluated')
+        self._parser.add_argument('--mode', type=str, default='Validation', choices=['Validation', 'Test'], help='Whether \
+        	evaluate it on the validation set or the test set.')
+        self._parser.add_argument('--ensemble', action='store_true')
+        self._parser.add_argument("--n_students", type=int, default=5)
+
+        self.is_train = False
+
---
+++ Multitask-CNN/options/train_options.py
@@ -0,0 +1,19 @@
+from .base_options import BaseOptions
+
+
+class TrainOptions(BaseOptions):
+    def initialize(self):
+        BaseOptions.initialize(self)
+        self._parser.add_argument('--print_freq_s', type=int, default= 5, help='frequency of showing training results on console')
+        self._parser.add_argument('--lr_policy', type=str, default = 'step', choices = ['lambda', 'step', 'plateau'])
+        self._parser.add_argument('--lr_decay_epochs', type=int, default= 3, help="learning rate decays by 0.1 after every # epochs (lr_policy is 'step')")
+        self._parser.add_argument('--teacher_nepochs', type=int, default= 3, help='# of epochs to train')
+        self._parser.add_argument('--student_nepochs', type=int, default= 3, help='# of epochs for student to train')
+        self._parser.add_argument('--n_students', type=int, default= 5, help='# of students')
+        self._parser.add_argument('--lr_F', type=float, default=0.0001, help='initial learning rate for G adam')
+        self._parser.add_argument('--F_adam_b1', type=float, default=0.5, help='beta1 for G adam')
+        self._parser.add_argument('--F_adam_b2', type=float, default=0.999, help='beta2 for G adam')
+        self._parser.add_argument('--optimizer', type=str, default='Adam', choices = ['Adam', 'SGD'])
+
+
+        self.is_train = True
