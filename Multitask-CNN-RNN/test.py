08906e2f888add9fa8b479e812fa63c87d170e44
Didan Deng <33117903+wtomin@users.noreply.github.com>
2020-02-08 22:05:30 +0800

    Add files via upload

--- Multitask-CNN-RNN/test.py
+++ Multitask-CNN-RNN/test.py
@@ -1,61 +1,161 @@
 import time
 from options.test_options import TestOptions
-from data.custom_dataset_data_loader import CustomDatasetDataLoader
+from data.test_video_dataset import Test_dataset
 from models.models import ModelsFactory
 from collections import OrderedDict
 import os
 import numpy as np
+import torch
 from sklearn.metrics import f1_score
 from PATH import PATH
 import pandas as pd
 from tqdm import tqdm
 from copy import deepcopy
+from scipy.stats import mode
+from scipy.special import softmax
+import pickle
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+PRESET_VARS = PATH()
+#################RuntimeError: received 0 items of ancdata ###########################
+torch.multiprocessing.set_sharing_strategy("file_system")
+#########################################################################
 class Tester:
     def __init__(self):
         self._opt = TestOptions().parse()
-        PRESET_VARS = PATH()
         self._model = ModelsFactory.get_by_name(self._opt.model_name, self._opt)
-        self.has_label = True 
-        self.datasets_names = {"AU": self._opt.AU_dataset, "EXPR": self._opt.EXPR_dataset, 'VA':self._opt.VA_dataset}
-        val_transforms = self._model.current_model.backbone.compose_transforms
-        self.test_dataloaders = dict([(k, CustomDatasetDataLoader(self._opt, 'Validation', self.datasets_names[k], transform = val_transforms).load_data()) for k in self._opt.test_tasks_seq])
-        print("Tasks\tDatasets\tTest Size(# of images)")
-        for k in self._opt.test_tasks_seq:
-            print("{}\t{}\t{}".format(k, self.datasets_names[k], 
-                len(self.test_dataloaders[k]) * self._opt.batch_size))
-        self._test()
+        test_data_file = PRESET_VARS.Aff_wild2.test_data_file
+        self.test_data_file = pickle.load(open(test_data_file, 'rb'))
+        self.save_dir = self._opt.save_dir
+        if not os.path.exists(self.save_dir):
+            os.makedirs(self.save_dir)
+        if self._opt.mode == 'Test':
+            self._test()
+        else:
+            raise ValueError("do not call test.py with validation mode.")
     def _test(self):
-        test_start_time = time.time()
         self._model.set_eval()
-        dicts = {}
-        for task in self._opt.test_tasks_seq:
-            print("Evaluate the current model in {} dataset".format(self.datasets_names[task]))
-            track_preds = []
-            track_labels = []
-            track_indexes = []
-            track_paths = []
-            for i_test_batch, test_batch in tqdm(enumerate(self.test_dataloaders[task]), total = len(self.test_dataloaders[task])):
-                outputs = self._model.inference_current(test_batch, task, return_estimates=True)
-                label = test_batch['label']
-                index = test_batch['index']
-                pred = outputs[task]
-                track_labels.append(deepcopy(label))
-                track_preds.append(deepcopy(pred))
-                track_indexes.append(deepcopy(index))
-                del test_batch, outputs
-            preds = np.concatenate(track_preds, axis=0)
-            labels = np.concatenate(track_labels, axis=0)
-            track_indexes = np.concatenate(track_indexes, axis=0)
-            data = dict([('{}'.format(task), preds), ('index',track_indexes)])
-            if self.has_label:
-                metric = self._model.get_metrics_per_task()[task]
-                eval_item, eval_res = metric(preds, labels)
-                data.update({'metric':eval_res, 'metric_0': eval_item[0], 'metric_1': eval_item[1]})
-                print("Evaluation Performanace for {}: res {}, res_0 {}, res_1 {}".format(task, eval_res, eval_item[0], eval_item[1]))
-            dicts[task] = data
-        save_path = os.path.join(self._opt.loggings_dir, self._opt.save_path)
-        pickle.dump(dicts, open(save_path, 'wb'))
+        val_transforms = self._model.resnet50.backbone.compose_transforms
+        if self._opt.eval_with_teacher:
+            model_paths = [self._opt.teacher_model_path]
+        else:
+            model_paths = []
+        if self._opt.ensemble:
+            for i in range(self._opt.n_students):
+                path = os.path.join(self._opt.checkpoints_dir, self._opt.name, 'net_epoch_student_{}_id_resnet50.pth'.format(i))
+                assert os.path.exists(path)
+                model_paths.append(path)
+        outputs_record = {}
+        estimates_record = {}
+        frames_ids_record = {}
+        for i, path in enumerate(model_paths):
+            self._model.resnet50.load_state_dict(torch.load(path))        
+            outputs_record[i] = {}
+            estimates_record[i] = {}
+            frames_ids_record[i] = {}
+            for task in self._opt.tasks:
+                task = task+"_Set"
+                task_data_file = self.test_data_file[task]['Test_Set']
+                outputs_record[i][task] = {}
+                estimates_record[i][task] = {}
+                frames_ids_record[i][task] = {}
+                for i_video, video in enumerate(task_data_file.keys()):
+                    video_data = task_data_file[video]
+                    test_dataset = Test_dataset(self._opt, video_data, transform=val_transforms)
+                    test_dataloader = torch.utils.data.DataLoader(
+                    test_dataset,
+                    batch_size=self._opt.batch_size,
+                    shuffle= False,
+                    num_workers=int(self._opt.n_threads_test),
+                    drop_last=False)
+                    track = self.test_one_video(test_dataloader, task = task[:-4])
+                    torch.cuda.empty_cache() 
+                    outputs_record[i][task][video] = track['outputs']
+                    estimates_record[i][task][video] = track['estimates']
+                    frames_ids_record[i][task][video] = track['frames_ids']
+                    print("Model ID {} Task {} Current {}/{}".format(i, task[:-4], i_video, len(task_data_file.keys())))
+                    save_path = '{}/{}/{}.txt'.format(i, task, video)
+                    self.save_to_file(track['frames_ids'], track['estimates'], save_path, task=task[:-4])
+                    # if i_video>=1:
+                    #     break
+        #merge the raw outputs 
+        for task in self._opt.tasks:
+            task = task+"_Set"
+            for video in outputs_record[0][task].keys():
+                preds = []
+                for i in range(len(outputs_record.keys())):
+                    preds.append(outputs_record[i][task][video])
+                preds = np.array(preds)
+                #assert frames_ids_record[0][task][video] == frames_ids_record[1][task][video]
+                video_frames_ids = frames_ids_record[0][task][video] 
+                if task == 'AU_Set':
+                    merged_preds = sigmoid(preds)
+                    best_thresholds_over_models = [0.1448537,  0.03918985, 0.13766725, 0.02652811, 0.40589422, 0.15572545,
+     0.04808964, 0.10848708]
+                    print("The best AU thresholds over models: {}".format(best_thresholds_over_models))
+                    merged_preds = np.mean(merged_preds, axis=0) 
+                    merged_preds = merged_preds > (np.ones_like(merged_preds)*best_thresholds_over_models)
+                    merged_preds = merged_preds.astype(np.int64)
+                    save_path = '{}/{}/{}.txt'.format('merged', task, video)
+                    self.save_to_file(video_frames_ids, merged_preds, save_path, task='AU')
+                elif task == 'EXPR_Set':
+                    merged_preds = softmax(preds, axis=-1).mean(0).argmax(-1).astype(np.int).squeeze()
+                    save_path = '{}/{}/{}.txt'.format('merged',task,  video)
+                    self.save_to_file(video_frames_ids, merged_preds, save_path, task='EXPR')
+                else:
+                    N = self._opt.digitize_num
+                    v = softmax(preds[:, :, :N], axis=-1)
+                    a = softmax(preds[:, :, N:], axis=-1)
+                    bins = np.linspace(-1, 1, num=self._opt.digitize_num)
+                    v = (bins * v).sum(-1)
+                    a = (bins * a).sum(-1)
+                    merged_preds = np.stack([v.mean(0), a.mean(0)], axis = 1).squeeze() 
+                    save_path = '{}/{}/{}.txt'.format( 'merged',task,  video) 
+                    self.save_to_file(video_frames_ids, merged_preds, save_path, task='VA')  
 
+    def save_to_file(self, frames_ids, predictions, save_path, task= 'AU'):
+        save_path =os.path.join(self.save_dir, save_path)
+        save_dir = os.path.dirname(os.path.abspath(save_path))
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        categories = PRESET_VARS.Aff_wild2.categories[task]
+        assert len(frames_ids) == len(predictions)
+        assert frames_ids[-1] == len(frames_ids) - 1
+        with open(save_path, 'w') as f:
+            f.write(",".join(categories)+"\n")
+            for i, line in enumerate(predictions):
+                if isinstance(line, np.ndarray):
+                    digits = []
+                    for x in line:
+                        if isinstance(x, float):
+                            digits.append("{:.4f}".format(x))
+                        elif isinstance(x, np.int64):
+                            digits.append(str(x))
+                    line = ','.join(digits)+'\n'
+                elif isinstance(line, np.int64):
+                    line = str(line)+'\n'
+                if i == len(predictions)-1:
+                    line = line[:-1]
+                f.write(line)
 
+    def test_one_video(self, data_loader, task = 'AU'):
+        track_val = {'outputs':[], 'estimates':[], 'frames_ids':[]}
+        for i_val_batch, val_batch in tqdm(enumerate(data_loader), total = len(data_loader)):
+            # evaluate model
+            wrapped_v_batch = {task: val_batch}
+            self._model.set_input(wrapped_v_batch, input_tasks = [task])
+            outputs, _ = self._model.forward(return_estimates=False, input_tasks = [task])
+            estimates, _ = self._model.forward(return_estimates=True, input_tasks = [task])
+            #store the predictions and labels
+            track_val['outputs'].append(outputs[task][task])
+            track_val['frames_ids'].append(np.array(val_batch['frames_ids']))
+            track_val['estimates'].append(estimates[task][task])
+             
+        for key in track_val.keys():
+            track_val[key] = np.concatenate(track_val[key], axis=0)
+        assert len(track_val['frames_ids']) -1 == track_val['frames_ids'][-1]
+        return track_val
 if __name__ == "__main__":
     Tester()
+            
+
---
+++ Multitask-CNN-RNN/val.py
@@ -0,0 +1,169 @@
+import time
+from options.test_options import TestOptions
+from data.custom_dataset_data_loader import Multitask_DatasetDataLoader
+from models.models import ModelsFactory
+from collections import OrderedDict
+import os
+import numpy as np
+import torch
+from sklearn.metrics import f1_score
+from PATH import PATH
+import pandas as pd
+from tqdm import tqdm
+from copy import deepcopy
+from scipy.stats import mode
+from scipy.special import softmax
+import math
+import pickle
+from sklearn.metrics import precision_recall_curve
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+#################RuntimeError: received 0 items of ancdata ###########################
+torch.multiprocessing.set_sharing_strategy("file_system")
+#########################################################################
+class Tester:
+    def __init__(self):
+        self._opt = TestOptions().parse()
+        PRESET_VARS = PATH()
+        self._model = ModelsFactory.get_by_name(self._opt.model_name, self._opt)
+        val_transforms = self._model.resnet50.backbone.compose_transforms
+        #self.test_dataloaders = dict([(k, CustomDatasetDataLoader(self._opt, self._opt.mode, self.datasets_names[k], transform = val_transforms).load_data()) for k in self._opt.test_tasks_seq])
+        self.validation_dataloaders = Multitask_DatasetDataLoader(self._opt, train_mode = self._opt.mode, transform = val_transforms)
+        self.validation_dataloaders = self.validation_dataloaders.load_multitask_val_test_data()
+        print("{} sets".format(self._opt.mode))
+        for task in self._opt.tasks:
+            data_loader = self.validation_dataloaders[task]
+            print("{}: {} images".format(task, len(data_loader)*self._opt.batch_size * len(self._opt.tasks)))
+        if self._opt.mode == 'Validation':
+            self._validate()
+        else:
+            raise ValueError("do not call val.py with test mode.")
+        
+    def _validate(self):
+        val_start_time = time.time()
+        # set model to eval
+        self._model.set_eval()
+        if self._opt.eval_with_teacher:
+            model_paths = [self._opt.teacher_model_path]
+        else:
+            model_paths = []
+        if self._opt.ensemble:
+            for i in range(self._opt.n_students):
+                path = os.path.join(self._opt.checkpoints_dir, self._opt.name, 'net_epoch_student_{}_id_resnet50.pth'.format(i))
+                assert os.path.exists(path)
+                model_paths.append(path)
+        print("Evaluation: {} models".format(len(model_paths)))
+        outputs_record = {}
+        estimates_record = {}
+        metrics_record = {}
+        labels_record = {}
+        for i, path in enumerate(model_paths):
+            self._model.resnet50.load_state_dict(torch.load(path))
+            outputs_record[i] = {}
+            estimates_record[i] = {}
+            metrics_record[i] = {}
+            labels_record[i] = {}
+            for task in self._opt.tasks:
+                track_val = {'outputs':[],'labels':[], 'estimates':[]}
+                data_loader = self.validation_dataloaders[task]
+                for i_val_batch, val_batch in tqdm(enumerate(data_loader), total = len(data_loader)):
+                    # evaluate model
+                    wrapped_v_batch = {task: val_batch}
+                    self._model.set_input(wrapped_v_batch, input_tasks = [task])
+                    outputs, _ = self._model.forward(return_estimates=False, input_tasks = [task])
+                    estimates, _ = self._model.forward(return_estimates=True, input_tasks = [task])
+                    #store the predictions and labels
+                    track_val['outputs'].append(outputs[task][task])
+                    track_val['labels'].append(wrapped_v_batch[task]['label'])
+                    track_val['estimates'].append(estimates[task][task])
+                    # if i_val_batch>10:
+                    #     break
+                # calculate metric
+                for key in track_val.keys():
+                    track_val[key] = np.concatenate(track_val[key], axis=0)
+                preds = track_val['estimates']
+                labels = track_val['labels']
+                metric_func = self._model.get_metrics_per_task()[task]
+                eval_items, eval_res = metric_func(preds, labels)
+                now_time = time.strftime("%H:%M", time.localtime(val_start_time))
+                output = "Model id {} {} Validation {}: Eval_0 {:.4f} Eval_1 {:.4f} eval_res {:.4f}".format(i, task, 
+                    now_time, eval_items[0], eval_items[1], eval_res)
+                print(output)
+                outputs_record[i][task] = track_val['outputs']
+                estimates_record[i][task] = track_val['estimates']
+                labels_record[i][task] = track_val['labels']
+                metrics_record[i][task] = [eval_items, eval_res]
+        # one choice, merge the estimates
+        for task in self._opt.tasks:
+            preds = []
+            labels = []
+            for i in range(len(estimates_record.keys())):
+                preds.append(estimates_record[i][task])
+                labels.append(labels_record[i][task])
+            preds = np.array(preds)
+            labels = np.array(labels)
+            #assert labels[0] == labels[1]
+            if task == 'AU' or task == 'EXPR':
+                merged_preds = mode(preds, axis=0)[0]
+            elif task == 'VA':
+                merged_preds = np.mean(preds, axis=0)
+            labels = np.mean(labels,axis=0)
+            metric_func = self._model.get_metrics_per_task()[task]
+            eval_items, eval_res = metric_func(merged_preds.squeeze(), labels.squeeze())
+            now_time = time.strftime("%H:%M", time.localtime(val_start_time))
+            output = "Merged First method {} Validation {}: Eval_0 {:.4f} Eval_1 {:.4f} eval_res {:.4f}".format( task, 
+                now_time, eval_items[0], eval_items[1], eval_res)
+            print(output)
+        # one choice, average the raw outputs, this one is better than the first one
+        for task in self._opt.tasks:
+            preds = []
+            labels = []
+            for i in range(len(estimates_record.keys())):
+                preds.append(outputs_record[i][task])
+                labels.append(labels_record[i][task])
+            preds = np.array(preds)
+            labels = np.array(labels)
+            #assert labels[0] == labels[1]
+            if task == 'AU':
+                merged_preds = sigmoid(preds)
+                best_thresholds_over_models = []
+                for i in range(len(merged_preds)):
+                    f1_optimal_thresholds = []
+                    merged_preds_per_model = merged_preds[i]
+                    for j in range(merged_preds_per_model.shape[1]):
+                        precision, recall, thresholds = precision_recall_curve(labels[i][:, j].astype(np.int),merged_preds[i][:, j])
+                        f1_optimal_thresholds.append(thresholds[np.abs(precision-recall).argmin(0)])
+                    f1_optimal_thresholds = np.array(f1_optimal_thresholds)
+                best_thresholds_over_models.append(f1_optimal_thresholds)
+                best_thresholds_over_models = np.array(best_thresholds_over_models).mean(0)
+                merged_preds = np.mean(merged_preds, axis=0) 
+                merged_preds = merged_preds > (np.ones_like(merged_preds)*best_thresholds_over_models)
+                merged_preds = merged_preds.astype(np.int64)
+                print("The best AU thresholds over models: {}".format(best_thresholds_over_models))
+            elif task=='EXPR':
+                merged_preds = softmax(preds, axis=-1).mean(0).argmax(-1).astype(np.int)
+            else:
+                N = self._opt.digitize_num
+                v = softmax(preds[:, :, :N], axis=-1).argmax(-1)
+                a = softmax(preds[:, :, N:], axis=-1).argmax(-1)
+                v = mode(v, axis=0)[0]
+                a = mode(a, axis=0)[0]
+                v = np.eye(N)[v]
+                a = np.eye(N)[a]
+                bins = np.linspace(-1, 1, num=self._opt.digitize_num)
+                v = (bins * v).sum(-1)
+                a = (bins * a).sum(-1)
+                merged_preds = np.stack([v.squeeze(), a.squeeze()], axis = 1)
+            labels = np.mean(labels, axis=0)
+            metric_func = self._model.get_metrics_per_task()[task]
+            eval_items, eval_res = metric_func(merged_preds.squeeze(), labels.squeeze())
+            now_time = time.strftime("%H:%M", time.localtime(val_start_time))
+            output = "Merged Second method {} Validation {}: Eval_0 {:.4f} Eval_1 {:.4f} eval_res {:.4f}".format(task, 
+                now_time, eval_items[0], eval_items[1], eval_res)
+            print(output)
+        save_path = 'evaluate_val_set.pkl'
+        data = {'outputs':outputs_record, 'estimates':estimates_record, 'labels':labels_record, 'metrics':metrics_record}
+        pickle.dump(data, open(save_path, 'wb'))
+
+if __name__ == "__main__":
+    Tester()
