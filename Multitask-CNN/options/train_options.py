c8b4c4984d83f78c8115f52558596c96573021d4
Didan Deng <33117903+wtomin@users.noreply.github.com>
2020-02-20 17:13:18 +0800

    Update train_options.py

--- Multitask-CNN/options/train_options.py
+++ Multitask-CNN/options/train_options.py
@@ -7,7 +7,7 @@ class TrainOptions(BaseOptions):
         self._parser.add_argument('--print_freq_s', type=int, default= 5, help='frequency of showing training results on console')
         self._parser.add_argument('--lr_policy', type=str, default = 'step', choices = ['lambda', 'step', 'plateau'])
         self._parser.add_argument('--lr_decay_epochs', type=int, default= 3, help="learning rate decays by 0.1 after every # epochs (lr_policy is 'step')")
-        self._parser.add_argument('--teacher_nepochs', type=int, default= 3, help='# of epochs to train')
+        self._parser.add_argument('--teacher_nepochs', type=int, default= 8, help='# of epochs to train')
         self._parser.add_argument('--student_nepochs', type=int, default= 3, help='# of epochs for student to train')
         self._parser.add_argument('--n_students', type=int, default= 5, help='# of students')
         self._parser.add_argument('--lr_F', type=float, default=0.0001, help='initial learning rate for G adam')
